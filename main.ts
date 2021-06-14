74743e3313a41bea9ca6d287ca8411eb92be61b4
colinjaques <72202216+colinjaques@users.noreply.github.com>
2021-06-14 13:49:26 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -26,3 +26,35 @@ basic.pause(1000)
 basic.showNumber(1)
 basic.pause(1000)
 basic.showString("KABOOM!")
+
+//3 - Blinking Heart
+basic.forever(function () {
+    basic.showLeds(`
+. # . # .
+# . # . #
+# . # . #
+. # . # .
+. . # . .
+`)
+basic.pause(500)
+basic.showLeds(`
+. . . . .
+. . . . .
+. . . . .
+. . . . .
+. . . . .
+`)
+basic.pause(500)
+
+})
+
+//4 - Score Tracker
+let score = 0
+input.onButtonPressed(Button.A, function () {
+    score = score + 1
+    basic.showNumber(score)
+})
+input.onButtonPressed(Button.B, function () {
+    score = score - 1
+    basic.showNumber(score)
+})
