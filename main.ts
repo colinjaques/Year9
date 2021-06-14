5ade0ef47a7b2f74ef4d07218b7e4bedff30efbd
colinjaques <72202216+colinjaques@users.noreply.github.com>
2021-06-14 13:43:04 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+
+//1 - Output
 basic.showString("Hello!", 50)
 basic.showString("Mr Jaques", 75)
 basic.showString("This is my first program", 50)
+
+//2 - Countdown Timer
+basic.showNumber(10)
+basic.pause(1000)
+basic.showNumber(9)
+basic.pause(1000)
+basic.showNumber(8)
+basic.pause(1000)
+basic.showNumber(7)
+basic.pause(1000)
+basic.showNumber(6)
+basic.pause(1000)
+basic.showNumber(5)
+basic.pause(1000)
+basic.showNumber(4)
+basic.pause(1000)
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+basic.showString("KABOOM!")
